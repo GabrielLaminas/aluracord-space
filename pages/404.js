02f541be9dd6bd1b0792f55763de91540fbcdfc7
Gabriel de Freitas Laminas <laminas533@gmail.com>
2022-01-25 11:20:15 -0300

    final 404

--- pages/404.js
+++ pages/404.js
@@ -1,22 +1,39 @@
-import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import { Box, Button, Image } from '@skynexui/components';
+import { useRouter } from 'next/router';
 
 export default function PaginaErro(){
+  const roteamento = useRouter();
+  console.log(roteamento)
   return (
     <>
       <Box
         styleSheet={{
-          display: 'flex', alignItems: 'center', justifyContent: 'center',
+          display: 'flex', 
+          alignItems: 'center', 
+          justifyContent: 'center',
+          flexDirection: 'column',
           backgroundColor: "#FFFFFF",
         }}
       >
-          <Box  
-            styleSheet={{
-              width: '550px',
-              height: '550px',
-              backgroundImage: 'url()',
-              backgroundRepeat: 'no-repeat', backgroundSize: 'cover',
-            }}
-          />
+        <Image
+          src='https://raw.githubusercontent.com/GabrielLaminas/aluracord-space/9d1b8537b04a86907ae1cec29eb630a7063119e4/public/Monster%20404%20Error-rafiki.svg'
+          styleSheet={{
+            width: '530px',
+          }}
+        />
+        <Button
+          iconName="home"
+          type='submit'
+          label='Página Inicial'
+          onClick={() => roteamento.push('/')}
+          buttonColors={{
+            contrastColor: '#FDFDFD',
+            mainColor: '#2f2f47',
+            mainColorLight: '#414162',
+            mainColorStrong: '#414162',
+          }}
+        >
+        </Button>
       </Box>
     </>
   );
