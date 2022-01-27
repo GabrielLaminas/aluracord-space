5f0b38a1e2030a9b28e35fcde8cedf47834f1e2c
Gabriel de Freitas Laminas <laminas533@gmail.com>
2022-01-27 18:12:59 -0300

    env

--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  env: {
-    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzI4Mzk3MiwiZXhwIjoxOTU4ODU5OTcyfQ.gLMEXx2qODLUAtWu6ph4fE7XLFJSna_bWbg9m3-b5cs',
-    SUPABASE_URL: 'https://oigzyahvxvkcijusqdhy.supabase.co',
-  },
-}
